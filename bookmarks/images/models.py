9d405852edf763c4ae65585f89ff800009b3c75c
Esaul1391 <1391esaul@gmail.com>
2024-05-20 06:21:26 +0300

    create forms

--- bookmarks/images/models.py
+++ bookmarks/images/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.utils.text import slugify
+from django.urls import reverse
 
 
 class Image(models.Model):
@@ -31,4 +32,7 @@ class Image(models.Model):
         if not self.slug:
             self.slug = slugify(self.title)
         super().save(*args, **kwargs)
-# Create your models here.
+
+    def get_absolute_url(self):
+        return reverse('images:detail', args=[self.id, self.slug])
+
